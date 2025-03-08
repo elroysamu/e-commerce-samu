4cdd9682b275c16fc3f6c852fe4192c479e908a3
elroysamu <elroysamu@gmail.com>
2025-03-08 11:44:08 +0530

    added signin and signup endpoints

--- src/main/java/com/E_Commerce/repositories/UserRepository.java
+++ src/main/java/com/E_Commerce/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.E_Commerce.repositories;
 
 import com.E_Commerce.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,  Long> {
 
     Optional<User> findByUserName(String name);
+
+    Boolean existsByUserName(String username);
+
+    Boolean existsByEmail(String email);
 }
